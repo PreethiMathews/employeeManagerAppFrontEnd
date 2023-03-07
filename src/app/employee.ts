730c40e9778de460134e347824220866f924312d
PreethiMathews <mathewspreethi99@gmail.com>
2023-03-07 23:01:23 +0530

    First Commit

---
+++ src/app/employee.ts
@@ -0,0 +1,9 @@
+export interface Employee {
+    id: number;
+    empName: string;
+    email: string;
+    jobTitle: string;
+   // phone: string;
+    imageURL: string;
+    employeeCode: string;
+}
